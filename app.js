18df4ee56b4a0811008c9906b92c848d3b518d10
João Felipe <118225101+JoaoGomesDev32@users.noreply.github.com>
2025-04-26 13:58:58 +0100

    :rocket:
    configurar grupo de rotas no express

--- app.js
+++ app.js
@@ -1,24 +1,39 @@
 //Carregando módulos
 const express = require('express');
-const handlebars = require('express-handlebars');
+const { engine } = require('express-handlebars');
 const bodyParser = require('body-parser');
 const app = express();
+const admin = require('./routes/admin');
 //const mongoose = require('mongoose');
 
 //Configurações
-//Body Parser
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
 //Handlebars
-app.engine('handlebars', handlebars({defaultLayout: 'main'}));
+app.engine('handlebars', engine({
+    defaultLayout: 'main',
+    runtimeOptions: {
+        allowProtoPropertiesByDefault: true,
+        allowProtoMethodsByDefault: true
+    }
+}));
 app.set('view engine', 'handlebars');
+//Body Parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 //Mongoose
 //EM BREVE
 
 //Rotas
+app.get('/', (req, res) => {
+    res.send('Rota principal!');
+});
 
+app.get('/posts', (req, res) => {
+    res.send('Lista de posts!');
+});
+
+app.use('/admin', admin);
 //Outros
 const PORT = 8081
-app.listen(PORT, () =>{
+app.listen(PORT, () => {
     console.log('Servidor rodando!');
 })
